aa9b2e74acbfb1b48bc11548df15193b8cce3620
idoheart <3cupsroasting@gmail.com>
2025-03-29 13:12:00 +1100

    fixed package name

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "idoheart-ios",
-            targets: ["idoheart-ios"]),
+            name: "IDoHeart",
+            targets: ["IDoHeart"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "idoheart-ios"),
+            name: "IDoHeart"),
 
     ]
 )
